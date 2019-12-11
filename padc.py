4328d03105fe8249647f06b35858bd3587a5ffea
Jonathan Metzgar <microwerx@outlook.com>
2019-12-10 15:39:51 -0900

    match jupyter notebook

--- padc.py
+++ padc.py
@@ -1,22 +1,12 @@
-import numpy
+import numpy as np
+import urllib.request
 import json
 import matplotlib.pyplot as plt
-import numpy as np
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
-from io import StringIO
-import urllib.request
+EXTENTS = [-172, -128, 40, 90]
 
-class TSDataPoint:
-    def __init__(self, ID: int):
-        self.ID = ID
-        # self.pm010 = channelA.field1
-        # self.pm025 = channelA.field2
-        # self.pm100 = channelA.field3
-        # self.uptimeInMinutes = channelA.field4
-        # self.rssi = channelA.field5
-        # self.temperatureF = channelA.field6
-        # self.humidityPct = channelA.field7
-        # self.pm25_ug_m3 = channelA.field8
 
 class PADataPoint:
     # sensor A data fields
@@ -60,30 +50,82 @@ class PADataPoint:
         self.adc = data[32]
         self.ch = data[33]
 
+    def plot(self, plt, style: str):
+        if self.good():
+            plt.plot(self.lon, self.lat, style)
+        else:
+            print('Error! --> ' + str(self))
+
+    def good(self):
+        return self.lon != None and self.lat != None
+
     def __str__(self):
         return str(self.id) + ' ' + str(self.lat) + ' ' + str(self.lon) + ' ' + str(self.pm)
 
+
 class PAData:
-    def __init__(self, data):
-        self.version = data.get('version')
-        self.fields = data.get('fields')
-        self.count = data.get('count')
-        self.dataPoints = []
-        for d in data.get('data'):
-            self.dataPoints.append(PADataPoint(d))
+    def __init__(self, url: str):
+        self.download(url)
 
     def print(self):
         for d in self.dataPoints:
             print(d)
 
-def is_inside_alaska():
-    return True;
+    def download(self, url: str):
+        try:
+            response = urllib.request.urlopen(url)
+        except HTTPError as e:
+            print('Error code: ', e.code)
+            return
+        http_data = response.read()
+        json_data = json.loads(http_data)
+        self.process(json_data)
 
-def read_current_purple_air(url: str):
-    response = urllib.request.urlopen(url)
-    http_data = response.read()
-    json_data = json.loads(http_data)
-    return PAData(json_data)
+    def process(self, data):
+        self.version = data.get('version')
+        self.fields = data.get('fields')
+        self.count = data.get('count')
+        self.dataPoints = []
+        fout = open('padata.txt', 'w')
+        for d in data.get('data'):
+            dp = PADataPoint(d)
+            if dp.lat == None or dp.lon == None:
+                continue
+            if (dp.lat < EXTENTS[2] or dp.lat > EXTENTS[3]):
+                continue
+            if (dp.lon < EXTENTS[0] or dp.lon > EXTENTS[1]):
+                continue
+            self.dataPoints.append(dp)
+            fout.write(str(dp) + '\n')
+        fout.close()
+        print("Added " + str(len(self.dataPoints)) + " data points")
 
-# pad = read_current_purple_air('https://www.purpleair.com/data.json')
-# pad.print()
+    def plot(self, plt, style):
+        for d in self.dataPoints:
+            d.plot(plt, style)
+
+
+def plotAlaska():
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    # ax = plt.axes(projection=ccrs.Orthographic(-153.369141, 66.160507))
+    ax.set_extent([-172, -128, 40, 90])
+    ax.coastlines()
+    ax.stock_img()
+    ax.gridlines()
+
+    states_provinces = cfeature.NaturalEarthFeature(
+        category='cultural',
+        name='admin_1_states_provinces_lines',
+        scale='50m',
+        facecolor='none')
+
+    ax.add_feature(cfeature.LAND)
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(states_provinces, edgecolor='gray')
+
+
+pad = PAData('https://www.purpleair.com/data.json')
+
+plotAlaska()
+pad.plot(plt, 'r+')
+plt.show()
