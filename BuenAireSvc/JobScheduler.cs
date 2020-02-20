b62eaaebbdebc5fe61df5e4a85cc53db5692e2aa
Mark Underwood <mtunderwood@alaska.edu>
2020-02-19 21:45:09 -0900

    Initial commit

---
+++ BuenAireSvc/JobScheduler.cs
@@ -0,0 +1,35 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuenAireSvc
+{
+    public class JobScheduler
+    {
+        public async static void Start()
+        {
+            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            IScheduler scheduler = await schedulerFactory.GetScheduler();
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<SensorFetchJob>()
+                .WithIdentity("purpleair-sensor-fetch")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     s.WithIntervalInHours(24)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 0))
+                  )
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+            await scheduler.TriggerJob(new JobKey("purpleair-sensor-fetch"));
+        }
+    }
+}
