dc1032f7c79f28da6b0dde6a8ff429145b7a95dc
Simeon Ng <ssng@alaska.edu>
2020-02-24 17:36:46 -0900

    Add icons for MasterPage

--- src/xamarin/buenaire/buenaire/buenaire.Android/Resources/Resource.designer.cs
+++ src/xamarin/buenaire/buenaire/buenaire.Android/Resources/Resource.designer.cs
@@ -5975,10 +5975,16 @@ namespace buenaire.Droid
 			public const int notify_panel_notification_icon_bg = 2131165320;
 			
 			// aapt resource value: 0x7F070089
-			public const int tooltip_frame_dark = 2131165321;
+			public const int purpleair = 2131165321;
 			
 			// aapt resource value: 0x7F07008A
-			public const int tooltip_frame_light = 2131165322;
+			public const int tooltip_frame_dark = 2131165322;
+			
+			// aapt resource value: 0x7F07008B
+			public const int tooltip_frame_light = 2131165323;
+			
+			// aapt resource value: 0x7F07008C
+			public const int uafsmoke = 2131165324;
 			
 			static Drawable()
 			{
