0bd0c1053926f391b7336472dad607e0bb0dc99b
Simeon Ng <ssng@alaska.edu>
2020-02-24 16:24:54 -0900

    Add dummy pages for future maps

--- src/xamarin/buenaire/buenaire/buenaire/MainPage.xaml.cs
+++ src/xamarin/buenaire/buenaire/buenaire/MainPage.xaml.cs
@@ -18,6 +18,24 @@ namespace buenaire
         public MainPage()
         {
             InitializeComponent();
+
+            masterPage.listView.ItemSelected += OnItemSelected;
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                MasterBehavior = MasterBehavior.Popover;
+            }
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
         }
     }
 }
