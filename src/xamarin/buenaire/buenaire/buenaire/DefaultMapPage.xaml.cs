beb71c48facca492521889485630c0d817093e9a
Simeon Ng <ssng@alaska.edu>
2020-02-24 15:41:37 -0900

    Move default map to own page

---
+++ src/xamarin/buenaire/buenaire/buenaire/DefaultMapPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
+
+namespace buenaire
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DefaultMapPage : ContentPage
+    {
+        public DefaultMapPage()
+        {
+            InitializeComponent();
+
+            Map map = new Map(MapSpan.FromCenterAndRadius(new Position(64.83, -147.71), Distance.FromMiles(10)));
+
+            Pin pin = new Pin()
+            {
+                Position = new Position(64.83, -147.71),
+                Label = "Fairbanks!"
+            };
+
+            map.Pins.Add(pin);
+
+            Content = map;
+        }
+    }
+}
--- src/xamarin/buenaire/buenaire/buenaire/MainPage.xaml.cs
+++ src/xamarin/buenaire/buenaire/buenaire/MainPage.xaml.cs
@@ -13,26 +13,11 @@ namespace buenaire
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void map_Clicked(object sender, EventArgs e)
-        {
-            Map map = new Map(MapSpan.FromCenterAndRadius(new Position(64.83, -147.71), Distance.FromMiles(10)));
-
-            Pin pin = new Pin()
-            {
-                Position = new Position(64.83, -147.71),
-                Label = "Fairbanks!"
-            };
-
-            map.Pins.Add(pin);
-
-            Content = map;
         }
     }
 }
