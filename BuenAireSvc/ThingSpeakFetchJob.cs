d08c1ef8c807a0802f9c428be0f1810d29f9bed9
Mark Underwood <mtunderwood@alaska.edu>
2020-02-26 11:02:56 -0900

    Recreate splash screen

---
+++ BuenAireSvc/ThingSpeakFetchJob.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BuenAireSvc
+{
+    public class ThingSpeakFetch
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        async public static Task getSensorData(string ID, string ApiKey)
+        {
+            var response = await client.GetStringAsync("https://api.thingspeak.com/channels/" + ID + "/feeds.json?api_key=" + ApiKey);
+
+            if (response != null)
+            {
+                JObject obj = JObject.Parse(response);
+
+                var pm2point5 =
+                    from entry in obj["feeds"]
+                    select entry;
+
+                File.WriteAllText(@".\Data\Feeds\" + ID + ".json", JsonConvert.SerializeObject(pm2point5));
+            }
+        }
+    }
+}
--- src/xamarin/buenaire/buenaire/buenaire.Android/MainActivity.cs
+++ src/xamarin/buenaire/buenaire/buenaire.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace buenaire.Droid
 {
-    [Activity(Label = "buenaire", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "buenaire", Icon = "@mipmap/icon", Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -17,6 +17,7 @@ namespace buenaire.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
+            base.SetTheme(Resource.Style.MainTheme);
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
