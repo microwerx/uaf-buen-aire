7b9d3573d8d22cc9f53a0d614af1baf5c24e01bb
Joseph  Byam <josephbyam@Josephs-MacBook-Pro.local>
2020-02-27 14:56:40 -0900

    Made a purple air map and put some colored shapes on it.

--- src/xamarin/buenaire/buenaire/buenaire/PurpleAirPage.xaml.cs
+++ src/xamarin/buenaire/buenaire/buenaire/PurpleAirPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
 
 namespace buenaire
 {
@@ -15,6 +16,72 @@ namespace buenaire
         public PurpleAirPage()
         {
             InitializeComponent();
+
+            Map map = new Map(MapSpan.FromCenterAndRadius(new Position(64.83, -147.71), Distance.FromMiles(10)));
+
+            Polygon polygon1 = new Polygon()
+            {
+                StrokeWidth = 25,
+                StrokeColor = Color.FromHex("#1BA1E2"),
+                FillColor = Color.FromHex("#881BA1E2"),
+                Geopath =
+                        {
+                            new Position(64.80, -147.71),
+                            new Position(64.78, -147.69),
+                            new Position(64.76, -147.71),
+                            new Position(64.78, -147.73)
+                        }
+            };
+
+            Polygon polygon2 = new Polygon()
+            {
+                StrokeWidth = 25,
+                StrokeColor = Color.DarkRed,
+                FillColor = Color.Red,
+                Geopath =
+                        {
+                            new Position(64.90, -147.71),
+                            new Position(64.88, -147.69),
+                            new Position(64.86, -147.71),
+                            new Position(64.88, -147.73)
+                        }
+            };
+
+            Polygon polygon3 = new Polygon()
+            {
+                StrokeWidth = 25,
+                StrokeColor = Color.DarkOrange,
+                FillColor = Color.Orange,
+                Geopath =
+                        {
+                            new Position(64.85, -147.76),
+                            new Position(64.83, -147.74),
+                            new Position(64.81, -147.76),
+                            new Position(64.83, -147.78)
+                        }
+            };
+
+            Polygon polygon4 = new Polygon()
+            {
+                StrokeWidth = 25,
+                StrokeColor = Color.Yellow,
+                FillColor = Color.LightYellow,
+                Geopath =
+                        {
+                            new Position(64.85, -147.66),
+                            new Position(64.83, -147.64),
+                            new Position(64.81, -147.66),
+                            new Position(64.83, -147.68)
+                        }
+            };
+
+            // add the polygon to the map's MapElements collection
+            map.MapElements.Add(polygon1);
+            map.MapElements.Add(polygon2);
+            map.MapElements.Add(polygon3);
+            map.MapElements.Add(polygon4);
+
+            Content = map;
         }
     }
 }
