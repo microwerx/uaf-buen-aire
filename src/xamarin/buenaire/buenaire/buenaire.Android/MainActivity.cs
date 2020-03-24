6dc2e40fdf35c9bfe0af5fe0fb563542e364ee8e
Joseph  Byam <josephbyam@Josephs-MacBook-Pro.local>
2020-03-24 11:46:51 -0800

    Moved some things around in the Android main file.

--- src/xamarin/buenaire/buenaire/buenaire.Android/MainActivity.cs
+++ src/xamarin/buenaire/buenaire/buenaire.Android/MainActivity.cs
@@ -22,12 +22,12 @@ namespace buenaire.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            Xamarin.FormsMaps.Init(this, savedInstanceState);
             var platformConfig = new PlatformConfig
             {
                 BitmapDescriptorFactory = new AccessNativeBitmapConfig()
             };
-            Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+            Xamarin.FormsGoogleMaps.Init(this, savedInstanceState, platformConfig);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
