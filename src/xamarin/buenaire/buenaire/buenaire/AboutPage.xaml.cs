139a4344c59a81b407c10d1f92c0c4bcef4d4c2d
Simeon Ng <ssng@alaska.edu>
2020-03-31 13:01:48 -0800

    Add AboutPage and Icon

---
+++ src/xamarin/buenaire/buenaire/buenaire/AboutPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace buenaire
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+        public AboutPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
