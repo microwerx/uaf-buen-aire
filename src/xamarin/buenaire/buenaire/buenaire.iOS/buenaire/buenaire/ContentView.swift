5d4bb5493c8cb8c6e44a79aaa17724fceb5209cf
llundell <llundell@ymail.com>
2020-04-08 16:45:41 -0800

    Adding files for iOS compilation

---
+++ src/xamarin/buenaire/buenaire/buenaire.iOS/buenaire/buenaire/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  buenaire
+//
+//  Created by Laura Lundell on 4/3/20.
+//  Copyright © 2020 Laura Lundell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
