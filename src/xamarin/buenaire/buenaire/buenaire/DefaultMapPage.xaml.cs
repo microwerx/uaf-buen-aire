7f8dfb221cf0e579667016c561b7b770eae3b753
jmbyam <jmbyam@alaska.edu>
2020-04-14 14:21:15 -0800

    I'm trying to give my stuff to you

--- src/xamarin/buenaire/buenaire/buenaire/DefaultMapPage.xaml.cs
+++ src/xamarin/buenaire/buenaire/buenaire/DefaultMapPage.xaml.cs
@@ -17,17 +17,91 @@ namespace buenaire
         {
             InitializeComponent();
 
-            Map map = new Map(MapSpan.FromCenterAndRadius(new Position(64.83, -147.71), Distance.FromMiles(10)));
+            CustomMap customMap = new CustomMap();
 
-            Pin pin = new Pin()
+            CustomPin pin1 = new CustomPin
             {
-                Position = new Position(64.83, -147.71),
-                Label = "Fairbanks!"
+                Type = PinType.Place,
+                Position = new Position(64.83d, -147.71d),
+                Label = "A Freaking Pin",
+                Name = "Xamarin",
+                Address = "There will be things said here",
+                IconType = 1,
+                Url = "https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#1/25/-30"
+            };
+            CustomPin pin2 = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(64.83d, -147.69d),
+                Label = "A Freaking Pin",
+                Address = "Maybe",
+                Name = "Xamarin",
+                IconType = 2,
+                Url = "https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#1/25/-30"
+            };
+            CustomPin pin3 = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(64.83d, -147.73d),
+                Label = "A Freaking Pin",
+                Address = "Maybe",
+                Name = "Xamarin",
+                IconType = 0,
+                Url = "https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#1/25/-30"
             };
 
-            map.Pins.Add(pin);
+            CustomPin pin4 = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(64.82d, -147.71d),
+                Label = "A Freaking Pin",
+                Name = "Xamarin",
+                Address = "There will be things said here",
+                IconType = 4,
+                Url = "https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#1/25/-30"
+            };
+            CustomPin pin5 = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(64.82d, -147.69d),
+                Label = "A Freaking Pin",
+                Address = "Maybe",
+                Name = "Xamarin",
+                IconType = 5,
+                Url = "https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#1/25/-30"
+            };
+            CustomPin pin6 = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(64.82d, -147.73d),
+                Label = "A Freaking Pin",
+                Address = "Maybe",
+                Name = "Xamarin",
+                IconType = 3,
+                Url = "https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#1/25/-30"
+            };
 
-            Content = map;
+            CustomPin pin7 = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(64.81d, -147.73d),
+                Label = "A Freaking Pin",
+                Address = "Maybe",
+                Name = "Xamarin",
+                IconType = 6,
+                Url = "https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#1/25/-30"
+            };
+            customMap.CustomPins = new List<CustomPin> { pin1, pin2, pin3, pin4, pin5, pin6, pin7 };
+            customMap.Pins.Add(pin1);
+            customMap.Pins.Add(pin2);
+            customMap.Pins.Add(pin3);
+            customMap.Pins.Add(pin4);
+            customMap.Pins.Add(pin5);
+            customMap.Pins.Add(pin6);
+            customMap.Pins.Add(pin7);
+            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(64.82d, -147.71d), Distance.FromMiles(1.0)));
+
+            Content = customMap;
         }
     }
 }
