3af22188551b781f84decd3fd9ce14c01ffad904
RobinTheSprite <madscientistak@gmail.com>
2020-04-22 22:11:41 -0800

    Add empty class for processing images

---
+++ BuenAireSvc/ImageStitcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BuenAireSvc
+{
+    public class ImageStitcher
+    {
+        public ImageStitcher()
+        {}
+
+        public void mergeTiles(string tilePath)
+        {
+
+        }
+
+        public void sliceTiles(FileStream image)
+        {
+
+        }
+    }
+}
