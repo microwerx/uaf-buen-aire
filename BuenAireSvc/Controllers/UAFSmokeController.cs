d78c9bae19cd6de09eb1b8fec76080e9ed149d70
RobinTheSprite <madscientistak@gmail.com>
2020-04-22 21:41:58 -0800

    Add controller for UAF Smoke

---
+++ BuenAireSvc/Controllers/UAFSmokeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http;
+
+namespace BuenAireSvc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UAFSmokeController : ControllerBase
+    {
+        [HttpGet("{zoom}/{x}/{y}")]
+        public ActionResult<FileStream> Get(uint zoom, int x, int y)
+        {
+            string physicalPath = $"Data/UAFSmoke/smokeL{zoom}T{x.ToString("D2")}{y.ToString("D2")}.png";
+            return System.IO.File.OpenRead(physicalPath);
+        }
+    }
+}
